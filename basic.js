154d1e089ed1d9f4995033dad889f2bac17b7d6e
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2022-04-07 00:32:58 +0600

    Complete grade check task

--- basic.js
+++ basic.js
@@ -1,94 +1,139 @@
-// Getting Started with basic Javascript
+// // Getting Started with basic Javascript
 
-// Any letter to uppercase
+// // Any letter to uppercase
 
-var promice = "this letter should be convert to uppercase";
+// var promice = "this letter should be convert to uppercase";
 
-var promice = promice.toUpperCase();
-promice = promice.toUpperCase();
+// var promice = promice.toUpperCase();
+// promice = promice.toUpperCase();
 
-console.log(promice);
+// console.log(promice);
 
-Any letter to Lowercase
+// // Any letter to Lowercase
 
-var low = "ANY LETTER SHOULD BE LOWERCASE";
-low = low.toLowerCase();
+// var low = "ANY LETTER SHOULD BE LOWERCASE";
+// low = low.toLowerCase();
 
-console.log(low);
+// console.log(low);
 
-var number = 22;
-var number2 = "55.33";
-number2 = parseInt(number2);
-var total = number + number2;
+// var number = 22;
+// var number2 = "55.33";
+// number2 = parseInt(number2);
+// var total = number + number2;
 
-console.log(total);
+// console.log(total);
 
-var number = 22;
-var number2 = "55.33";
-number2 = parseInt(number2);
-number = "" + 22;
-var total = number + number2;
+// var number = 22;
+// var number2 = "55.33";
+// number2 = parseInt(number2);
+// number = "" + 22;
+// var total = number + number2;
 
-console.log(total);
+// console.log(total);
 
-var num = .02;
-var num2 = .55;
-var total = num +num2;
-total = total.toFixed(2);
+// var num = .02;
+// var num2 = .55;
+// var total = num +num2;
+// total = total.toFixed(2);
 
-console.log(total);
+// console.log(total);
 
-var num = 90;
-var num2 = -555;
-var total = num +num2;
-var absulateNumbar = Math.abs(total);
+// var num = 90;
+// var num2 = -555;
+// var total = num +num2;
+// var absulateNumbar = Math.abs(total);
 
-console.log(absulateNumbar);
+// console.log(absulateNumbar);
 
-var num = 90;
-var num2 = -555.42;
-var total = num +num2;
-var total = Math.abs(total);
+// var num = 90;
+// var num2 = -555.42;
+// var total = num +num2;
+// var total = Math.abs(total);
 
-// var total = Math.round(total);
+// // var total = Math.round(total);
 
-// var total = Math.ceil(total);
+// // var total = Math.ceil(total);
 
-var total = Math.floor(total);
+// var total = Math.floor(total);
 
-console.log(total);
+// console.log(total);
 
-var noodlsePrice = 22;
+// var noodlsePrice = 22;
 
-if (noodlsePrice < 20) {
-  console.log("You Can Buy This")
-} 
-else {
-  console.log("Don't Buy")
-}
-
-
-var noodlsePrice = 22;
-if (noodlsePrice == 20) {
-  console.log("You Can Buy This");
-} 
-else {
-  console.log("Don't Buy");
-}
+// if (noodlsePrice < 20) {
+//   console.log("You Can Buy This")
+// } 
+// else {
+//   console.log("Don't Buy")
+// }
 
 
-var hardWorking = yes;
-var Millionheir = yes;
+// var noodlsePrice = 22;
+// if (noodlsePrice == 20) {
+//   console.log("You Can Buy This");
+// } 
+// else {
+//   console.log("Don't Buy");
+// }
 
 
-if (noodlsePrice != 20) {
+// var hardWorking = yes;
+// var Millionheir = yes;
 
-  console.log("You Can Buy This");
 
-} 
+// if (noodlsePrice != 20) {
+
+//   console.log("You Can Buy This");
+
+// } 
  
-else {
-  console.log("Don't Buy");
-}
+// else {
+//   console.log("Don't Buy");
+// }
+
+
 
 // Starting First javaScript Project Check Grade git
+
+var marks = 70;
+
+
+if (marks>=80 && marks<=100) {
+  console.log("Grade A+");
+  
+} 
+
+else if (marks>=70 && marks<=79) {
+  console.log("Grade A");
+  
+} 
+
+else if (marks>=60 && marks<=69) {
+  console.log("Grade A-");
+  
+} 
+
+else if (marks>=50 && marks<=59) {
+  console.log("Grade B");
+  
+} 
+
+else if (marks>=40 && marks<=49) {
+  console.log("Grade C");
+  
+} 
+
+else if (marks>=33 && marks<=39) {
+  console.log("Grade D");
+  
+} 
+
+else if (marks>=1 && marks<=32) {
+  console.log("Fail");
+  
+} 
+
+else {
+  console.log("Inveiled Entry");
+  
+}
