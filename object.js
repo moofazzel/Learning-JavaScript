23e12d5cadea5baa8ebe0efa4d99cdc5482b8a7a
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2022-04-15 03:03:14 +0600

    Getting basic javascript of variable, function, loop, array, object etc

---
+++ object.js
@@ -0,0 +1,19 @@
+var student = {id:16, phone:01938688488, name:"rony"}
+var student2 = {id:33, phone:488, name:"imran"}
+
+// console.log(student, student2);
+
+var studentPhone = student.phone;
+var studentPhone1 = student2["phone"];
+var studentPhone1 = student2["phone"];
+
+
+student.phone = 645456456465;
+// Update element
+student2["phone"] = 44411122;
+// Add element 
+student2["new bike"] = "GSXR";
+
+
+console.log(studentPhone);
+console.log(student2);
